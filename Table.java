5c5d2489c36646cb61d2394601ed5e98fe077a53
Keerthiga-I <keerthigairisan15@gmail.com>
2025-03-27 13:01:13 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -14,10 +14,10 @@ public static void main (String[] args)
         int n = Integer.parseInt(s1);
         System.out.println("5th TABLE");
         System.out.println("-----------------------");
-		for(int i=1;i<=10;i++)
-		{
-			System.out.println(i+"x"+n+"="+(i*n));
-		}
+	for(int i=1;i<=10;i++)
+	{
+		System.out.println(i+"x"+n+"="+(i*n));
+	}
 
 	}
 	catch(Exception e)
